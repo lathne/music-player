520b32a66c3084fd9ead08a3a269a92c5e4e709b
Laura TN <62152335+lathne@users.noreply.github.com>
2020-09-04 20:32:46 -0300

    added some javaScript and some HTML changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const songs = [
-    "beautiful village",
-    "childhood nostalgia",
-    "elven forest",
-    "forest ventures",
-    "relaxing nature",
-    "western adventures"
+    "beautiful-village.mp3",
+    "childhood-nostalgia.mp3",
+    "elven-forest.mp3",
+    "forest-ventures.mp3",
+    "relaxing-nature.mp3",
+    "western-adventures.mp3"
 ]
 //creating an ordered list of items with each of my songs
 const createSongList = () => {
@@ -20,3 +20,26 @@ const createSongList = () => {
 }
 
 document.getElementById('songList').appendChild(createSongList())
+
+songList.onclick = (e) => {
+    const clickedItem = e.target //each time a list item is clicked on let's call it a clicledItem
+    const source = document.getElementById('source')
+    source.src = 'songs/' + clickedItem.innerText
+    console.log(clickedItem.innerText)
+
+    document.getElementById('currentlyPlayingSong').innerText = "Currently Playing: "
+    document.getElementById('currentSong').innerText = clickedItem.innerText
+
+    player.load()
+    player.play()
+}
+
+const playAudio = () => {
+    if(player.readyState) {
+        player.play()
+    }
+}
+
+const pauseAudio = () => {
+    player.pause()
+}
